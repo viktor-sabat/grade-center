9c9ecc6fcfd904facf297fbe5c536ecfb72e562c
JustCallMeQuincy <71931404+JustCallMeQuincy@users.noreply.github.com>
2024-05-11 14:08:31 +0200

    Now with updated assignment card and grid components

--- src/app/assignment-card/assignment-card.component.ts
+++ src/app/assignment-card/assignment-card.component.ts
@@ -10,8 +10,21 @@ import { Component } from '@angular/core';
 
 export class AssignmentCardComponent {
   // Boolean variables representing different states of the assignment card.
-  // Based on these variables, the colour of the card will be determined.
+  // Based on these variables, the colour of the card will be determined
   isAssignmentUploaded: boolean = false;
   isAssignmentReviewed: boolean = false;
   isAssignmentExpired: boolean = true;
+
+  // Date for deadlines in assignment card
+  deadlineDate: string = "20.04.2024";
+
+  // Represents the title of the assignment card
+  assignmentTitle: string = "Assignment Title..."
+
+  // Represents the status of the assignment card
+  assignmentStatusTitle: string = "Uploaded"
+  
+  // Represents the upload date on the assignment card
+  uploadDate: string = "19.04.2024"
+
 }
