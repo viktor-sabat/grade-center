e96aa0043c12f3d9eebf89708849d8bbc736db95
mewfive555 <akasha.solia@gmail.com>
2024-05-11 18:18:24 +0200

    edited app component and css files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import { AssignmentsGridComponent } from './assignments-grid/assignments-grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AssignmentsGridComponent],
+  imports: [RouterOutlet, SidemenuComponent, AssignmentsGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
