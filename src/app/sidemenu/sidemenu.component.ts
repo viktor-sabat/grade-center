698ed2d3414f1377349dd031b5a69f0f22ba4b6d
mewfive555 <akasha.solia@gmail.com>
2024-05-11 18:19:00 +0200

    added sidemenu component and edited app.route

---
+++ src/app/sidemenu/sidemenu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'tlr-sidemenu',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.css'
+})
+export class SidemenuComponent {
+
+}
