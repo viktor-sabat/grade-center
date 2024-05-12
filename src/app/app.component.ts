caa6859185223a469993bf4eded34e6e6acbacb9
JustCallMeQuincy <71931404+JustCallMeQuincy@users.noreply.github.com>
2024-05-12 17:33:42 +0200

    Now with new component for dropdown menu (+Viktor)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { UserHeaderComponent } from './user-header/user-header.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import { AssignmentsGridComponent } from './assignments-grid/assignments-grid.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserHeaderComponent, SidemenuComponent, AssignmentsGridComponent],
+  imports: [RouterOutlet, UserHeaderComponent, SidemenuComponent, AssignmentsGridComponent, DropdownComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
