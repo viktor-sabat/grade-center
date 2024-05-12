7366ce98ee8647249548c116f63980543e437efc
mewfive555 <akasha.solia@gmail.com>
2024-05-12 22:44:50 +0200

    edited app with Mathias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { UserHeaderComponent } from './user-header/user-header.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import { AssignmentsGridComponent } from './assignments-grid/assignments-grid.component';
 import { TaskFiltersComponent } from './task-filters/task-filters.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserHeaderComponent, SidemenuComponent, AssignmentsGridComponent, TaskFiltersComponent],
+  imports: [RouterOutlet, UserHeaderComponent, SidemenuComponent, AssignmentsGridComponent, TaskFiltersComponent, ContactPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AssignmentsGridComponent } from './assignments-grid/assignments-grid.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 export const routes: Routes = [
     {path: '', component: AssignmentsGridComponent},
-    {path: 'Tasks', component: AssignmentsGridComponent}
+    {path: 'Tasks', component: AssignmentsGridComponent},
+    {path: 'Contact', component: ContactPageComponent}
 ];
