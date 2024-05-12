389bcdc6dec30803615c61155edcd1a9dc2324e2
JustCallMeQuincy <71931404+JustCallMeQuincy@users.noreply.github.com>
2024-05-12 19:34:15 +0200

    Now with partial routing for tasks page (+Viktor)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { AssignmentsGridComponent } from './assignments-grid/assignments-grid.component';
+import { TaskFiltersComponent } from './task-filters/task-filters.component';
 
 export const routes: Routes = [
     {path: '', component: AssignmentsGridComponent},
-    {path: 'Tasks', component: AssignmentsGridComponent}
+    {path: 'Tasks', component: AssignmentsGridComponent},{path: 'Tasks', component: TaskFiltersComponent}
 ];
