d01fa5069708c0360c7da92cb461908d2c58b585
JustCallMeQuincy <71931404+JustCallMeQuincy@users.noreply.github.com>
2024-05-12 18:43:44 +0200

    Now with dropdown group component and styling (+Viktor)

---
+++ src/app/task-filters/task-filters.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TaskFiltersComponent } from './task-filters.component';
+
+describe('TaskFiltersComponent', () => {
+  let component: TaskFiltersComponent;
+  let fixture: ComponentFixture<TaskFiltersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TaskFiltersComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TaskFiltersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/task-filters/task-filters.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DropdownComponent } from '../dropdown/dropdown.component';
+
+@Component({
+  selector: 'tlr-task-filters',
+  standalone: true,
+  imports: [DropdownComponent],
+  templateUrl: './task-filters.component.html',
+  styleUrl: './task-filters.component.css'
+})
+export class TaskFiltersComponent {
+
+}
