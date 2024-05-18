50e87aea051d8b0a556f5a998fa7004ce44881cb
mewfive555 <akasha.solia@gmail.com>
2024-05-18 16:25:17 +0200

    created sidemenu expended component

---
+++ src/app/sidemenu-expended/sidemenu-expended.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemenu-expended',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidemenu-expended.component.html',
+  styleUrl: './sidemenu-expended.component.css'
+})
+export class SidemenuExpendedComponent {
+
+}
