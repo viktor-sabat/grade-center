ffe18d591a55c9b1e12ce0771c90269a87197d32
mewfive555 <akasha.solia@gmail.com>
2024-05-19 14:33:28 +0200

    edited some styles and html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserHeaderComponent } from './user-header/user-header.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { SidemenuExpendedComponent } from './sidemenu-expended/sidemenu-expended.component';
 import { TasksPageContentComponent } from './tasks-page-content/tasks-page-content.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-   imports: [RouterOutlet, UserHeaderComponent, SidemenuComponent, TasksPageContentComponent, ContactPageComponent],
+  imports: [RouterOutlet, UserHeaderComponent, SidemenuComponent, SidemenuExpendedComponent, TasksPageContentComponent, ContactPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
