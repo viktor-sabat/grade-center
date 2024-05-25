ebb08269384bdc52f47bb2652903555a049e3996
mewfive555 <akasha.solia@gmail.com>
2024-05-25 15:47:43 +0200

    changed app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserHeaderComponent } from './user-header/user-header.component';
-import { SidemenuComponent } from './sidemenu/sidemenu.component';
-import { SidemenuExpendedComponent } from './sidemenu-expended/sidemenu-expended.component';
+import { SidemenuLogicComponent } from './sidemenu-logic/sidemenu-logic.component';
 import { TasksPageContentComponent } from './tasks-page-content/tasks-page-content.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
@@ -10,7 +9,7 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserHeaderComponent, SidemenuComponent, SidemenuExpendedComponent, TasksPageContentComponent, ProfilePageComponent, ContactPageComponent],
+  imports: [RouterOutlet, UserHeaderComponent, SidemenuLogicComponent, TasksPageContentComponent, ProfilePageComponent, ContactPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
