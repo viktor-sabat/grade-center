1b459421200a813751c794225ed0edc84b27fa18
mewfive555 <akasha.solia@gmail.com>
2024-05-25 15:48:22 +0200

    changed sidemenu logic

--- src/app/sidemenu-logic/sidemenu-logic.component.ts
+++ src/app/sidemenu-logic/sidemenu-logic.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { SidemenuComponent } from '../sidemenu/sidemenu.component';
+import { SidemenuExpendedComponent } from '../sidemenu-expended/sidemenu-expended.component';
 
 @Component({
-  selector: 'app-sidemenu-logic',
+  selector: 'tlr-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [SidemenuComponent, SidemenuExpendedComponent],
   templateUrl: './sidemenu-logic.component.html',
   styleUrl: './sidemenu-logic.component.css'
 })
 export class SidemenuLogicComponent {
-
+  isExpended: boolean = true;
 }
