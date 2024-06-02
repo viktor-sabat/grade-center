279d0afe1d7dde3cc284ebc08df8a353b3f8d918
mewfive555 <akasha.solia@gmail.com>
2024-06-02 16:40:49 +0200

    added mobile menu

---
+++ src/app/mobile-menu/mobile-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'tlr-mobile-menu',
+  standalone: true,
+  imports: [MatTooltipModule, RouterLink],
+  templateUrl: './mobile-menu.component.html',
+  styleUrl: './mobile-menu.component.css'
+})
+export class MobileMenuComponent {
+
+}
