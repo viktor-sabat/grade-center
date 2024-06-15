59f3f807d356aa5e0e296e80ea150560f00016c0
mewfive555 <akasha.solia@gmail.com>
2024-06-15 21:15:52 +0200

    added temp field for teacher +quincy

---
+++ Client/src/app/models/ITeacher.ts
@@ -0,0 +1,11 @@
+export interface Teacher {
+  id: number;
+  name: string;
+  firstName: string;
+  phoneNumber: string;
+  email: string;
+  dateOfBirth: string;
+  academicYear: string;
+  address: string;
+  region: string;
+}
