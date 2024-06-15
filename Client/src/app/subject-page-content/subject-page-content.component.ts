dc49655539141153feeec4bd3cc4635c5161d9e2
mewfive555 <akasha.solia@gmail.com>
2024-06-15 23:06:21 +0200

    Added url safety for roles +quincy

--- Client/src/app/subject-page-content/subject-page-content.component.ts
+++ Client/src/app/subject-page-content/subject-page-content.component.ts
@@ -1,16 +1,18 @@
 import { Component, Output } from '@angular/core';
 import { subjectGridComponent } from '../subject-grid/subject-grid.component';
 import { SubjectFiltersComponent } from '../subject-filters/subject-filters.component';
+import { Error404Component } from '../error-404/error-404.component';
 
 @Component({
   selector: 'tlr-subject-page-content',
   standalone: true,
-  imports: [subjectGridComponent, SubjectFiltersComponent],
+  imports: [subjectGridComponent, SubjectFiltersComponent, Error404Component],
   templateUrl: './subject-page-content.component.html',
   styleUrl: './subject-page-content.component.css',
 })
 export class SubjectPageContentComponent {
   subjectsTotal: number = 0;
+  userRole: string | null = localStorage.getItem('userRole');
 
   updateSubjectsTotal(count: number) {
     this.subjectsTotal = count;
