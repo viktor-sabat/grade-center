7397577e148282908753feec7be6d002f0b4e8b2
viktor <vitasabat1@gmail.com>
2024-06-15 09:44:54 +0200

    Add logic that counts all assignments and reviewed ones

--- Client/src/app/assignments-grid/assignments-grid.component.ts
+++ Client/src/app/assignments-grid/assignments-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AssignmentCardComponent } from '../assignment-card/assignment-card.component';
 import { StudentService } from '../api-service/api.service';
 import { Assignment } from '../models/IAssignment';
@@ -14,57 +14,53 @@ import { HttpClient } from '@angular/common/http';
   providers: [HttpClient]
 })
 
-export class AssignmentsGridComponent implements OnInit{
-  // Represents an array with a set of three boolean variables to define the card status
-  AssignmentStatusParameters: undefined | boolean[][] = [
-    [false, false, false],
-    [false, false, false],
-    [true, true, false],
-    [true, false, false],
-    [true, false, false],
-    [true, true, false],
-    [true, true, false],
-    [true, false, false],
-    [false, false, false],
-    [true, false, false],
-    [true, true, false],
-    [true, false, false],
-    [false, false, true],
-    [false, false, false],
-    [true, true, false],
-    [true, true, false],
-    [false, false, false],
-    [true, false, false],
-    [true, true, false],
-    [false, false, false],
-    [false, false, false],
-    [true, true, false],
-    [true, false, false],
-    [false, false, true],
-    [true, true, false],
-    [true, false, false],
-    [true, true, false],
-    [true, true, false],
-    [false, false, false],
-    [true, true, false],
-    [true, false, false],
-    [false, false, true],
-    [true, true, false],
-    [true, true, false],
-    [true, true, false],
-  ];  
+export class AssignmentsGridComponent implements OnInit {
+
+// Emits the total number of assignments to the parent component.
+@Output() assignmentsLength = new EventEmitter<number>();
+
+// Emits the number of reviewed assignments to the parent component.
+@Output() reviewedAssignmentsLength = new EventEmitter<number>();
+
 
   // An array of assignments
   assignments: Assignment[] = [];
 
   // Injecting the student service
-  constructor(private studentService: StudentService){
-   }
+  constructor(private studentService: StudentService) {}
 
-
+   /*
+   * Fetches assignments from the student service when the component initializes.
+   * Subscribes to assignment data and updates the local assignments array.
+   * Emits assignment counts after updating assignments.
+   */
   ngOnInit() {
     this.studentService.getAssignments().subscribe((assignments) => {
       this.assignments = assignments;
+      this.emitAssignmentCounts();
     });
+  }
+
+  /*
+   * Emits the current counts of all assignments and reviewed assignments
+   * using the assignmentsLength and reviewedAssignmentsLength event emitters.
+   */
+  emitAssignmentCounts() {
+    this.assignmentsLength.emit(this.getAllAssignmentsCount());
+    this.reviewedAssignmentsLength.emit(this.getReviewedAssignmentsCount());
+  }
+
+  /*
+   * Returns the total number of assignments in the assignments array.
+   */
+  getAllAssignmentsCount(): number {
+    return this.assignments.length;
+  }
+
+  /*
+   * Returns the number of assignments that have been reviewed (where reviewed is true).
+   */
+  getReviewedAssignmentsCount(): number {
+    return this.assignments.filter(assignment => assignment.reviewed).length;
   }
 }
